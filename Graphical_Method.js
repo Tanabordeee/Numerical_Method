e0b9612fa11f775a8859ca8ff54b5611e7cb12da
Tanabordee Pateepbutsagon <102315593+Tanabordeee@users.noreply.github.com>
2024-07-13 16:00:03 +0700

    delete else if in loop 2 and change  output

--- Graphical_Method.js
+++ Graphical_Method.js
@@ -6,11 +6,9 @@ function calculate(x) {
 let closestValue1 = Infinity;
 let closestValue2 = Infinity;
 let ClosestAns1 = Infinity;
-let ClosestAns2 = Infinity;
 let x1 = 0;
 let x2 = 0;
 let xAns1 = 0;
-let xAns2 = 0;
 for(let i = 0; i <= 10 ; i++) {
     let result = calculate(i);
     if(Math.abs(result) < Math.abs(closestValue1)){
@@ -25,12 +23,8 @@ for(let i = 0; i <= 10 ; i++) {
 for(let j = x1 ; j <= x2 ; j+=0.000001){
     result = calculate(j);
     if(Math.abs(result) < Math.abs(ClosestAns1)){
-        ClosestAns2 = ClosestAns1;
         ClosestAns1 = result;
         xAns1 = j;
-    }else if (Math.abs(result) < Math.abs(ClosestAns2)){
-        ClosestAns2 = result;
-        xAns2 = j;
     }
 }
-console.log("[ " + xAns1 + "  " + xAns2 + " ]");
+console.log("Approach is = " , xAns1);
