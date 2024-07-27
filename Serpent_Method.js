c34bf6cb5c64eaf4abe7a05ad331893f3d41c037
Tanabordee Pateepbutsagon <102315593+Tanabordeee@users.noreply.github.com>
2024-07-28 00:41:55 +0700

    update logic

--- Serpent_Method.js
+++ Serpent_Method.js
@@ -1,18 +1,16 @@
 function calculate(x) {
-    return (1/2)*(x + (7/x));
+    return Math.pow(x , 2) - 7;
 }
 
 let x0 = 2; 
-let x1 = 28/3; 
+let x1 = 2.75; 
 let error = 1;
 let i = 0;
 
 while (error > 0.000001) {
     let f_x0 = calculate(x0);
     let f_x1 = calculate(x1);
-    
-    let new_x = x1 - f_x1 * (x1 - x0) / (f_x1 - f_x0); 
-    
+    let new_x = x1 - (f_x1 * (x0 - x1) )/ (f_x0 - f_x1); 
     error = Math.abs(new_x - x1);
     console.log("Iteration", i, "X =", new_x);
     x0 = x1;
