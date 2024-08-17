ea3156a71d421103a4cbf725c4c3db4440651d13
Tanabordee Pateepbutsagon <102315593+Tanabordeee@users.noreply.github.com>
2024-08-17 20:57:28 +0700

    GuassElimination

--- GaussElimination.js
+++ GaussElimination.js
@@ -1,4 +1,16 @@
-let A = [[3,2,1],
-         [2,5,3],
-         [4,7,5]]
-         
+let A = [
+    [3, 2, 1],
+    [2, 5, 3],
+    [4, 7, 5],
+];
+
+let n = A.length;
+
+for (let i = 0; i < n; i++) {
+    for (let k = i + 1; k < n; k++) {
+        let factor = A[k][i] / A[i][i]; 
+        for (let j = 0; j < n; j++) {
+            A[k][j] -= factor * A[i][j];
+        }
+    }
+}
