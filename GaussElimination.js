6cda2a2de565c6bbbf9dc0a5d0b2954c004e4627
Tanabordee Pateepbutsagon <102315593+Tanabordeee@users.noreply.github.com>
2024-08-17 22:19:46 +0700

    guassElimination

--- GaussElimination.js
+++ GaussElimination.js
@@ -1,16 +1,46 @@
 let A = [
-    [3, 2, 1],
-    [2, 5, 3],
-    [4, 7, 5],
+    [4, -4, 0],
+    [-1, 4, -2],
+    [0, -2, 4],
 ];
+let B = [400 , 400 , 400];
 
 let n = A.length;
-
+let factor = []
+let result = []
+let temp = []
+let factorB = []
+let tempB = B.map(row => row);
 for (let i = 0; i < n; i++) {
-    for (let k = i + 1; k < n; k++) {
-        let factor = A[k][i] / A[i][i]; 
-        for (let j = 0; j < n; j++) {
-            A[k][j] -= factor * A[i][j];
+    for (let k = i+1; k < n; k++) {
+        for(let m = 0 ; m < n ; m++){
+            factor.push(A[i][m] / A[i][i]);
+            factorB.push(B[i] / A[i][i]);
         }
+        for (let j = 0; j < n; j++) {
+            temp[j] = A[k][j] - factor[j] * A[k][i];
+            tempB[k] = B[k] - factorB[j] * A[k][i];
+        }
+        for(let p = 0 ; p < n ; p++){
+            A[k][p] = temp[p];
+            B[p] = tempB[p];
+        }
+        factor = []
+        factorB = []
     }
 }
+let resultX = []
+// an * Xn = bn
+// Xn = bn/an
+// ai,i * Xi + ai,i+1 * Xi+1 + ... + ai,n * Xn = bi
+// Xi = (bi-(ai,i+1 * Xi+1+...+ai,n * Xn))/ai,i
+for(let i = n - 1 ; i >= 0 ; i--){
+    resultX[i] = B[i];
+    for(let j = i+1; j < n; j++){
+        resultX[i] -= A[i][j] * resultX[j];
+    }
+    resultX[i] /= A[i][i];
+}
+console.log("X1:", resultX[0]);
+console.log("X2:", resultX[1]);
+console.log("X3:", resultX[2]);
