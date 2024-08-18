ffce92926a4fdaccbfedcce5ded20d9bf1ff89d1
Tanabordee Pateepbutsagon <102315593+Tanabordeee@users.noreply.github.com>
2024-08-18 14:00:15 +0700

    Cholesky

---
+++ Cholesky.js
@@ -0,0 +1,51 @@
+const math = require('mathjs');
+
+let A = [
+    [4, 3, 1],
+    [3, 5, 2],
+    [1, 2, 6]
+];
+let B = [3125, 3650, 2800];
+
+let n = A.length;
+let L = math.zeros(n, n).valueOf();
+
+for(let k= 0 ;k< n ; k++){
+    for(let i = 0 ; i <= k ; i++){ 
+        let sum = 0;
+        for(let j = 0; j < i; j++){
+            sum += L[i][j] * L[k][j]; 
+        }
+
+        if(k == i){
+            L[k][k] = Math.sqrt(A[k][k] - sum);
+        } else {
+            L[k][i] = (A[k][i] - sum) / L[i][i];
+        }
+    }
+}
+// หา T Transpose
+let L_transpose = math.transpose(L);
+
+
+//L * Y = B
+let Y = []
+for (let i = 0; i < n; i++) {
+    let sum = 0;
+    for (let j = 0; j < i; j++) {
+        sum += L[i][j] * Y[j];
+    }
+    Y[i] = (B[i] - sum) / L[i][i];
+}
+
+//L^T * X = Y
+let X = []
+for (let i = n - 1; i >= 0; i--) {
+    let sum = 0;
+    for (let j = i + 1; j < n; j++) {
+        sum += L_transpose[i][j] * X[j];
+    }
+    X[i] = (Y[i] - sum) / L_transpose[i][i];
+}
+
+console.log('X = ', X);
