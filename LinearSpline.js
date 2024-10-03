4d1354374cf31988f0c153aff315a9f97b47fb77
Tanabordee Pateepbutsagon <102315593+Tanabordeee@users.noreply.github.com>
2024-10-03 10:32:13 +0700

    update LinearSpline

--- LinearSpline.js
+++ LinearSpline.js
@@ -4,16 +4,14 @@ let value = 4.5;
 let count = 0;
 let index1 = 0;
 let index2 = 0;
+let m =[]
+for(let i = 0 ; i < x.length - 1; i++){
+    m.push((y[i + 1]-y[i]) / (x[i + 1] - x[i]))
+}
 for(let i = 0 ; i < x.length; i++){
-    if(x[i] >= value){
-        index1 = i;
-        index2 = i - 1;
+    if(value >= x[i] && value <= x[i + 1]){
+        let result = y[i] + (m[i]) * (value - x[i]);
+        console.log(result);
         break;
     }
-}
-if(index2 < 0){
-    console.log("Value is smaller than the smallest x value");
-}else{
-    let result = y[index1] + ((y[index2]-y[index1]) / (x[index2] - x[index1])) * (value - x[index1]);
-    console.log(result);
 }
