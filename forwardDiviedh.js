9c50fcd9359a6dc8537c211e2503d35bdc981f3d
Tanabordee Pateepbutsagon <102315593+Tanabordeee@users.noreply.github.com>
2024-10-12 20:36:50 +0700

    update forward

--- forwardDiviedh.js
+++ forwardDiviedh.js
@@ -1,8 +1,21 @@
 const math = require('mathjs');
 let x = 2;
 let h = 0.25;
-let func = (x) => {math.pow(Math.E, x)};
+let func = (x) => { return math.pow(Math.E, x)};
 let diff1 = (x) => {
     return (func(x + h) - func(x)) / h;
 }
-let diff2
+let diff2 = (x) =>{
+    return (func(x + h*2) - 2*func(x+h) + func(x)) / math.pow(h , 2);
+}
+let diff3 = (x) =>{
+    return (func(x + h*3) - 3*func(x+h*2) + 3*func(x+h) - func(x)) / math.pow(h , 3);
+}
+let diff4 = (x) =>{
+    return (func(x + h*4) - 4*func(x+h*3) + 6*func(x+h*2) - 4*func(x+h) +func(x))/ math.pow(h , 4);
+}
+let symbolicDiff1 = math.derivative('exp(x)', 'x').evaluate({x: x});
+
+let error1 = math.abs((diff1(x) - symbolicDiff1) / diff1(x)) * 100 / 100;
+console.log(error1);
+console.log(diff1(x))
